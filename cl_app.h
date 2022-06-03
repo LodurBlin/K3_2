2b7d9ff9632015863a91030660b36a02e8edb50d
LodurBlin <105871932+LodurBlin@users.noreply.github.com>
2022-06-03 21:04:46 +0300

    Update cl_app.h

--- cl_app.h
+++ cl_app.h
@@ -2,15 +2,16 @@
 #define CL_APP_H
 #include "cl_base.h"
 #include <iostream>
-class cl_app : public cl_base 
+class cl_app : public cl_base //1 
 {
-	public:
-		cl_app() : cl_base("") {};
-		void derevo(); //postr
-		void print() {  //zapuck
-			std::cout << "Object tree";
-			print_hierarchy(0);
-		};
+public:
+	cl_app() : cl_base("") {};
+	void derevo(); //postr
+	void print() {  //zapuck
+		std::cout << "Object tree";
+		print_hierarchy(0);
+		questions();
+	};
 };
 
 #endif
