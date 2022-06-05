c5c372e90b477e94a9db4cce4024fc93296d121a
LodurBlin <105871932+LodurBlin@users.noreply.github.com>
2022-06-05 14:37:19 +0300

    Update cl_base.h

--- cl_base.h
+++ cl_base.h
@@ -10,6 +10,7 @@ class cl_base {
 
 public:
 	cl_base* p_predok{}; //указатель на голову
+
 	std::vector<std::unique_ptr<cl_base>> spinogrizi; //массив указателей
 
 	cl_base(std::string name, cl_base* p_predok = 0) : ob_name{ name }, p_predok{ p_predok } {}; //конструктор
@@ -18,7 +19,12 @@ public:
 		ob_name = name;
 	}
 	std::string get_name() const { //получение имени
-		return ob_name;
+		if (this) {
+			return ob_name;
+		}
+		else {
+			return "";
+		}
 	}
 	void add_spinogriz(std::unique_ptr<cl_base>&&); //добавление дочернего объекта
 
@@ -32,8 +38,5 @@ public:
 	cl_base* search(std::string wanted); //поиск объекта по имени
 
 	cl_base* findIP(std::vector<std::string> IP);//метод получения указателя на любой объект в составе дерева иерархии объектов согласно пути (координаты)
-
-	void questions();
-	std::string define(std::string IP, std::string cur);
 };
 #endif
