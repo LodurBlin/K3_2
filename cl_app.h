0abff6e7555645788dc52e7677c2b9f40f6b8507
LodurBlin <105871932+LodurBlin@users.noreply.github.com>
2022-06-05 14:36:54 +0300

    Update cl_app.h

--- cl_app.h
+++ cl_app.h
@@ -4,14 +4,21 @@
 #include <iostream>
 class cl_app : public cl_base //1 
 {
+private:
+	std::string main_root;
 public:
 	cl_app() : cl_base("") {};
 	void derevo(); //postr
 	void print() {  //zapuck
 		std::cout << "Object tree";
 		print_hierarchy(0);
-		questions();
+		if (main_root != "") {
+			std::cout << "\n";
+			questions();
+		}
 	};
+	std::string define(std::string IP, std::string cur);
+	void questions();
 };
 
 #endif
